07801f6b72397dfaf3b809f0197f2d493ce7ddf1
jarvis <168496656+jarvis630@users.noreply.github.com>
2025-01-27 13:42:40 +0530

    Update client.c
    changed the code from accepting the ip address to not accept it.

--- udp-factorial/client.c
+++ udp-factorial/client.c
@@ -1,31 +1,24 @@
 #include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
+#include <netinet/in.h> 
+#include <netdb.h> 
+#include <strings.h> 
 
-int main() {
+int main() { 
     int clientsocket, port, num;
-    struct sockaddr_in serveraddr;
-    socklen_t len;
-    char buffer[256];
+    struct sockaddr_in serveraddr; 
+    socklen_t len; 
+    char buffer[256]; 
 
-    clientsocket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (clientsocket < 0) {
-        perror("Error opening socket");
-        exit(1);
-    }
+    clientsocket = socket(AF_INET, SOCK_DGRAM, 0); 
 
-    bzero((char*)&serveraddr, sizeof(serveraddr));
-    printf("Enter the server's IP address: ");
-    char server_ip[50];
-    scanf("%s", server_ip);
-    printf("Enter the port number: ");
-    scanf("%d", &port);
-
+    bzero((char*)&serveraddr, sizeof(serveraddr)); 
+    len = sizeof(serveraddr); 
     serveraddr.sin_family = AF_INET;
-    serveraddr.sin_port = htons(port);
-    serveraddr.sin_addr.s_addr = inet_addr(server_ip);
+
+    printf("Enter the port number: "); 
+    scanf("%d", &port); 
+    serveraddr.sin_port = htons(port); 
+    serveraddr.sin_addr.s_addr = INADDR_ANY;
 
     printf("Enter a number to calculate factorial: ");
     scanf("%d", &num);
@@ -33,7 +26,8 @@ int main() {
     snprintf(buffer, sizeof(buffer), "%d", num);
     sendto(clientsocket, buffer, strlen(buffer), 0, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
 
-    len = sizeof(serveraddr);
+    printf("\nWaiting for server response...\n");
+
     bzero(buffer, 256);
     recvfrom(clientsocket, buffer, 255, 0, (struct sockaddr*)&serveraddr, &len);
     printf("Received from server: %s\n", buffer);
